9f19ec3a9aae354f6ae1ae5e65377358dbf980df
Tumezub <80384477+Tumezub@users.noreply.github.com>
2021-10-26 10:38:38 +0300

    Add files via upload

---
+++ EkatHarjoitukset/ReadLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjoita nimesi seuraavalle riville");
+            string Nimi = Console.ReadLine();
+            Console.WriteLine("Hei " + Nimi);
+        }
+    }
+}
