cb0d3d4e8e8a5dca8e59fad62ac8b1b92b08cc57
Tumezub <80384477+Tumezub@users.noreply.github.com>
2021-10-26 10:35:37 +0300

    Add files via upload

---
+++ TokatHarjoitukset/LaskuHarjoituksia.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace LaskuHarjoituksia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna luku");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku");
+            int luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("x = " + (luku2 + 3));
+            Console.WriteLine("Anna luku");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("x = " + (luku2 - 2));
+            Console.WriteLine("Anna luku");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("x = " + (luku2 * 5));
+            Console.WriteLine("Anna luku");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("x = " + (luku1 / luku2));
+            Console.WriteLine("Anna luku");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("x = " + (luku1 % luku2));
+            Console.WriteLine("Anna luku");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("x = " + (luku1 += luku2));
+            Console.WriteLine("Anna luku");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("x = " + (luku1 -= luku2));
+            Console.WriteLine("Anna luku");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("x = " + (luku1 *= luku2 * 5));
+            Console.WriteLine("Anna luku");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("x = " + (luku1 /= luku2));
+        }
+    }
+}
