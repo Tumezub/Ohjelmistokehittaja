c24b35bca6823b718dabf4367a57f2b1036464c6
Tumezub <80384477+Tumezub@users.noreply.github.com>
2021-12-10 11:45:46 +0200

    Add files via upload

---
+++ OlioTehtava/olioTehtava/KIRJASTO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace olioTehtava
+{
+    class KIRJASTO
+    {
+        public string kirjanNimi;
+        public string kirjailija;
+        public int sivuMaara;
+        public string lainassa;
+
+        public string KirjanNimi
+        {
+            get { return kirjanNimi; }
+            set
+            {
+                kirjanNimi = value;
+            }
+        }
+        public string Kirjailija
+        {
+            get { return kirjailija; }
+            set
+            {
+                kirjailija = value;
+            }
+        }
+        public int SivuMaara
+        {
+            get { return sivuMaara; }
+            set
+            {
+                sivuMaara = value;
+            }
+        }
+        public void lainaa()
+        {
+            lainassa = "Kyllä";
+        }
+        public void palauta()
+        {
+            lainassa = "Ei";
+        }
+    }
+}
---
+++ OlioTehtava/olioTehtava/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace olioTehtava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KIRJASTO kirjasto = new KIRJASTO();
+
+            kirjasto.kirjanNimi = "Tuntematon Kimi Räikkönen";
+            kirjasto.kirjailija = "Kari Hotakainen";
+            kirjasto.sivuMaara = 269;
+            kirjasto.lainassa = "Ei";
+            string knimi = kirjasto.kirjanNimi;
+            string kirjailija = kirjasto.kirjailija;
+            int smaara = kirjasto.sivuMaara;
+            string laina = kirjasto.lainassa;
+            Console.WriteLine("Kirjan nimi: " + knimi);
+            Console.WriteLine("Kirjailijan nimi: " + kirjailija);
+            Console.WriteLine("Sivumäärä: " + smaara);
+            HYPPY:
+            Console.WriteLine("Onko kirja lainassa tällä hetkellä? " + laina);
+            HYPPY2:
+            Console.WriteLine("Mitä haluat tehdä kirjalle?(L = Lainata, P = Palauttaa, E = En mitään)");
+            string vastaus = Console.ReadLine();
+            if(vastaus == "L")
+            {
+                kirjasto.lainaa();
+                laina = kirjasto.lainassa;
+                goto HYPPY;
+            }
+            else if(vastaus == "P")
+            {
+                kirjasto.palauta();
+                laina = kirjasto.lainassa;
+                goto HYPPY;
+            }
+            else if(vastaus == "E")
+            {
+                Console.WriteLine("Hyvää päivän jatkoa!");
+            }
+            else
+            {
+                Console.WriteLine("Virhe syötteessä. Annoithan varmasti oikean kirjaimen isona?");
+                goto HYPPY2;
+            }
+        }
+    }
+}
